bf74b3f710d2c3c6d5907bd3a355be96088ae0c4
Martin Nuc <martin@nuc.cz>
2016-10-29 19:42:40 +0200

    Display ut logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import {Component} from '@angular/core';
     <div class="container">
       <router-outlet></router-outlet>
     </div>
+    <a href="https://usertechnologies.com" target="blank">
+      <div class="ut-logo floating"></div>
+    </a>
   `
 })
 export class AppComponent {
